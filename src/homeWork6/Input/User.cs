cce10e6f4a0bc2d12de18288e381e30a637167ba
Anton Adamenko <adamanton1@ya.ru>
2019-09-08 00:21:16 +0700

    Добавил password в конструктор User

--- src/homeWork6/Input/User.cs
+++ src/homeWork6/Input/User.cs
@@ -13,16 +13,19 @@ namespace homeWork6.Input
         public string Email { private set; get; }
         public string Login { private set; get; }
 
-        public User(string name, int age, string email, string login)
+        public string Password { private set; get; }
+
+        public User(string name, int age, string email, string login, string password)
         {
             Name = name;
             Age = age;
             Email = email;
             Login = login;
+            Password = password;
         }
         public void Print()
         {
-            Console.WriteLine($"имя: {Name} возраст: {Age} Email {Email} Login {Login}");
+            Console.WriteLine($"имя: {Name} возраст: {Age} Email {Email} Login {Login} Password {Password}");
         }
 
     }
