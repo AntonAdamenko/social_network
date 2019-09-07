856be0135a528902d5bcc5588562fc640a1e980d
Anton Adamenko <adamanton1@ya.ru>
2019-09-07 22:02:50 +0700

    Добавил класс User

---
+++ src/homeWork6/Input/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork6.Input
+{
+    class User
+    {
+        public string Name { private set; get; }
+        public int Age { private set; get; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void PrintDate()
+        {
+            Console.WriteLine($"имя: {Name} возраст: {Age}");
+        }
+
+    }
+}
