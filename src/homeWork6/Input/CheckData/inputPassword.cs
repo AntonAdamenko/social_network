001b66813ec6e7fbe470aae02b1362e54df37d05
Anton Adamenko <adamanton1@ya.ru>
2019-09-08 00:17:51 +0700

    Добавил класс Password

---
+++ src/homeWork6/Input/CheckData/inputPassword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork6.Input.CheckData
+{
+    class inputPassword
+    {
+        public static string inputPasswordUser()
+        {
+             Console.WriteLine("Введите пароль");
+            string userPassword = "";
+            while (true)
+            {
+                char userChar = Console.ReadKey(true).KeyChar;
+                userPassword += userChar;
+                Console.Write("*");
+
+                if (userChar == '\r')
+                {
+                    Console.WriteLine("\n Ваш пароль: " + userPassword);
+                    break;
+                }
+            }
+            return userPassword;
+
+        }
+    }
+}
