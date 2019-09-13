285e5bce8f61b24eedef42d6f10f223dc370f566
Anton Adamenko <adamanton1@ya.ru>
2019-09-13 22:34:49 +0700

    Добавил валидацию пароля.
    Пустая строка или нет, длина пароля более 6 ти символов, есть ли в пароле большая буква, есть ли в  пароле цифра.

--- src/homeWork6/Input/CheckData/inputPassword.cs
+++ src/homeWork6/Input/CheckData/inputPassword.cs
@@ -11,26 +11,61 @@ namespace homeWork6.Input.CheckData
         public static string inputPasswordUser()
         {
             Console.WriteLine("Введите пароль");
-            string userPassword = "";            
-            
+            string userPassword = "";
+
             bool passwordIsValid = false;
             do
             {
                 userPassword = Console.ReadLine();
-                int lengthUserPassword = userPassword.Length;
-                if (lengthUserPassword >= 6)
-                {                    
-                    passwordIsValid = true;
+
+                if (string.IsNullOrEmpty(userPassword))
+                {
+                    Console.WriteLine("Это пустая строка. Введите еще раз");
+                }
+                else if (userPassword.Length <= 6)
+                {
+                    Console.WriteLine("Пароль должен содержать минимум 6 символов");
+                }
+                else if (IsNumberContains(userPassword) == false)
+                {
+                    Console.WriteLine("Пароль должен содержать минимум одну цифру");
+                }
+                else if (IsCapitalLetter(userPassword) == false)
+                {
+                    Console.WriteLine("Пароль должен содержать минимум одну большую букву");
                 }
                 else
-                {                    
-                    Console.WriteLine("Пароль короткий. Придумайте длинней");
-                }
-                
-
-
+                {
+                    passwordIsValid = true;
+                }                                            
             } while (!passwordIsValid);
             return userPassword;
+
+
+            
+
+            bool IsNumberContains(string str)
+            {
+                {
+                    foreach (char c in str)
+                        if (Char.IsNumber(c))
+                            return true;                    
+                }
+                return false;
+            }
+            bool IsCapitalLetter(string str)
+            {
+                {
+                    foreach (char c in str)
+                    {
+                        if (Char.IsUpper(c))
+                            return true;
+                    }
+                    return false;                    
+                }               
+            }
+
+
 
         }
     }
