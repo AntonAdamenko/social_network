f4e17c7451fdc64e7f61d37db00825d8985c72fa
Anton Adamenko <adamanton1@ya.ru>
2019-09-15 17:56:49 +0700

    Добавил валидацию email.
    При отсутствии символа @ вылетает ошибка. Необходимо обработать ее.

--- src/homeWork6/Input/CheckData/inputEmail.cs
+++ src/homeWork6/Input/CheckData/inputEmail.cs
@@ -17,33 +17,57 @@ namespace homeWork6.Input.CheckData
                 Console.WriteLine("Введите Email");
                 email = Console.ReadLine();
 
+                /*
+                //todo: обработать ошибку отсутствия символа @ в строке.
+                try
+                {
+                    if (!email.Contains('@'))
+                    {
+
+                    }
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("нет такого элемента");
+                }
+                */
+
+
+                int emalCharIndex = email.IndexOf("@");
+                string rightPart = email.Substring(emalCharIndex, email.Length - emalCharIndex);
+
 
                 if (string.IsNullOrEmpty(email))
                 {
                     Console.WriteLine("Это пустая строка. Enter-повторный ввод.Esc-выход");
-
+                }                
+                else if (!email.Contains('@'))
+                {
+                    Console.WriteLine("Email введен не корректно.Enter-повторный ввод. Esc-выход");
+                }              
+                else if (rightPart.Length <= 2)
+                {
+                    Console.WriteLine("После символа @ необходимо указать доменное имя");                                       
                 }
-                else if (email.Contains('@') && email.Contains(".com"))
+                else if (email.Contains(".com") || email.Contains(".ru") || email.Contains(".travel"))
                 {
                     emailIsValid = true;
-                }
+                }               
                 else
                 {
-                    Console.WriteLine("email введен не корректно.Enter-повторный ввод. Esc-выход");
-                }
-
-
+                    Console.WriteLine("Email введен не корректно. Укажите домен (например: .com, .ru, .travel).  Enter-повторный ввод. Esc-выход");
+                }                                     
+                
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
                 if (keyInfo.Key == ConsoleKey.Escape)
                 {
                     break;
                 }
 
-
             } while (!emailIsValid);
             return email;
 
-
+           
 
         }
 
