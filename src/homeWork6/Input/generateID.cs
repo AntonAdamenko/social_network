a3c538edc33cd3bbac37906bbb2ce79aa5d836b6
Anton Adamenko <adamanton1@ya.ru>
2019-10-01 00:19:57 +0700

    Добавил класс Генерация уникального ID
    Как один из вариантов генерации ID

---
+++ src/homeWork6/Input/generateID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork6.Input
+{
+    class generateID
+    {       
+            public static string GenerateId()
+            {
+
+                byte[] buffer = Guid.NewGuid().ToByteArray();
+                var FormNumber = BitConverter.ToUInt32(buffer, 0) ^ BitConverter.ToUInt32(buffer, 4) ^ BitConverter.ToUInt32(buffer, 8) ^ BitConverter.ToUInt32(buffer, 12);
+                string ID = FormNumber.ToString("X");
+
+                return ID;
+
+            }
+        
+    }
+}
+
