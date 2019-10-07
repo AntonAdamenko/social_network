7e7935cc906678e932cfa83b6e850cae31fe28db
Anton Adamenko <adamanton1@ya.ru>
2019-10-07 22:29:33 +0700

    Заменил поиск пользователя в массиве.
    Вместо оператора foreach использовал поиск с использованием System.Linq. Класс passwordIsValid переименовал в hidePassword и перенес в отдельный класс.

--- src/homeWork6/Input/loginInSystem.cs
+++ src/homeWork6/Input/loginInSystem.cs
@@ -10,38 +10,30 @@ using homeWork6.Input.CheckData;
 namespace homeWork6.Input
 {
     class loginInSystem
-    {        
-
+    {    
         public static void loginIsValid()
         {
             string DBFilePath = @"C:\Users\Antonio\source\repos\social_network\src\UsersData.json";
             List<User> users = Db.LoadUsersFromFileStatic(DBFilePath);
+                                   
+            string getLogin = inputLogin.inputLoginUser();       
 
-
-            Console.WriteLine("Введите логин");
-            string getLogin = Console.ReadLine();           
-           
-
-            //User findUser = users.FirstOrDefault(item => item.Login == getLogin);             
-            
-
-
-            //todo: вместо foreach реализовать другой поиск пользователей в массиве.
-            foreach (User item in users)
-            {               
-                if (item.Login.Equals(getLogin))
+            User findUser = users.FirstOrDefault(item => item.Login == getLogin);         
+                                   
+                if (findUser.Login.Equals(getLogin))
                 {
+                    //todo: Скрыть пароль *.  hidePassword();
 
-                    //todo: Скрыть пароль *.  passwordIsValid();
                     Console.WriteLine("Введите пароль");
-                    string pass = Console.ReadLine();
-                    if (item.Password.Equals(pass))
+                    string pass = Console.ReadLine();                    
+
+                    if (findUser.Password.Equals(pass))
                     {                       
                         Console.WriteLine("Повторите пароль");
                         string userPass = Console.ReadLine();
                         if (pass.Equals(userPass))
                         {
-                            Console.WriteLine($"Добро пожаловать в систему {item.Name}");
+                            Console.WriteLine($"Добро пожаловать в систему {findUser.Name}");
                         }
                         else
                         {
@@ -54,9 +46,9 @@ namespace homeWork6.Input
                         Console.WriteLine("Введите Email");
                         string getEmail = Console.ReadLine();
 
-                        if (item.Email.Equals(getEmail))
+                        if (findUser.Email.Equals(getEmail))
                         {
-                            Console.WriteLine($"Инструкция смены пароля отправлена на почту:  {item.Email}");
+                            Console.WriteLine($"Инструкция смены пароля отправлена на почту:  {findUser.Email}");
                         }
                         else
                         {
@@ -67,33 +59,7 @@ namespace homeWork6.Input
                 else
                 {
                     Console.WriteLine("Такого пользователя нет. Зарегистрируйтесь");
-                }
-            }
-            
-            
-
-
-        }
-        public static string passwordIsValid()
-        {
-            Console.WriteLine("Введите пароль");
-            string userPassword = "";
-            while (true)
-            {
-                char userChar = Console.ReadKey(true).KeyChar;
-                userPassword += userChar;
-                Console.Write("*");
-                //todo: При логине: после ввода пароля при нажатии Enter в конце строки записываеться символ \r и не дает войти в систему.
-                if (userChar == '\r')
-                {
-                    Console.WriteLine("\n Ваш пароль: " + userPassword);
-                    break;
-                }
-            }
-            return userPassword;
-        }
-
-
+                }                    
+        }        
     }
-
 }
