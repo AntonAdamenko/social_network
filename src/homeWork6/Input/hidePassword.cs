df01a356cc13b6aed64bedbd599712f05dc922c5
Anton Adamenko <adamanton1@ya.ru>
2019-10-07 22:31:56 +0700

    Создал класс hidePassword.
    При вводе пароля в консоль выводятся "*".

---
+++ src/homeWork6/Input/hidePassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork6.Input
+{
+    class hidePassword
+    {
+        public static string hideUserPassword()
+        {
+            Console.WriteLine("Введите пароль");
+            string userPassword = "";
+            while (true)
+            {
+                char userChar = Console.ReadKey(true).KeyChar;
+                userPassword += userChar;
+                Console.Write("*");
+                //todo: При логине: после ввода пароля при нажатии Enter в конце строки записываеться символ \r и не дает войти в систему.
+                if (userChar.Equals(Convert.ToChar(ConsoleKey.Enter)))
+                {
+                    Console.WriteLine("\n Ваш пароль: " + userPassword);
+                    break;
+                }
+            }
+            return userPassword;
+        }
+    }
+}
