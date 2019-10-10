2454b7984d580fb40c81da7b21a5a8026b877e79
Anton Adamenko <adamanton1@ya.ru>
2019-10-10 22:17:56 +0700

    Создал класс userInSystemNow.
    Класс userInSystemNow: когда пользователь находиться в системе у него есть возможность удалить свой профиль и сменить пароль. Пока добавил возможность удаления пользователя  deleteUser.

---
+++ src/homeWork6/Input/userInSystemNow.cs
@@ -0,0 +1,69 @@
+﻿using homeWork6.Input.CheckData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork6.Input
+{
+    class userInSystemNow
+    {
+        enum Menu
+        {
+            deleteUser=1,
+            changePassword = 2
+        }
+
+
+        public static void userInSystem()
+        {
+            string DBFilePath = @"C:\Users\Antonio\source\repos\social_network\src\UsersData.json";
+            List<User> users = Db.LoadUsersFromFileStatic(DBFilePath);
+
+            
+            Console.WriteLine("Вы в системе. \n Нажмите 1 для удаления профиля. \n Нажмите 2 для того чтобы сменить пароль.");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            switch (userNumber)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Удаление профиля");
+                        deleteUser();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Смена пароля");
+
+                        break;
+                    }
+            }
+        }
+
+        public static void deleteUser()
+        {
+            string DBFilePath = @"C:\Users\Antonio\source\repos\social_network\src\UsersData.json";
+            List<User> users = Db.LoadUsersFromFileStatic(DBFilePath);
+
+            string getLogin = inputLogin.inputLoginUser();
+
+            User findUser = users.FirstOrDefault(item => item.Login == getLogin);
+            try
+            {
+                users.Remove(findUser);
+                Console.WriteLine($"Пользователь {findUser.Name} удален.");
+                Db.SaveUsersToFile(DBFilePath, users.ToArray());
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Такого пользователя нет");
+            }            
+        }
+        public static void changePassword()
+        {
+           
+        }
+    }
+}
