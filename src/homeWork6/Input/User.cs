5f11950aa6eb5fab07119810a340fcfe0a2e83f3
Anton Adamenko <adamanton1@ya.ru>
2019-11-09 23:12:08 +0700

    В конструктор User добавил свойство salt
    В БД записывается соль при регистрции.

--- src/homeWork6/Input/User.cs
+++ src/homeWork6/Input/User.cs
@@ -12,21 +12,23 @@ namespace homeWork6.Input
         public int Age { private set; get; }
         public string Email { private set; get; }
         public string Login { private set; get; }
+        public string Salt { private set; get; }
         public string Password { set; get; }
         public int ID { private set; get; }
 
-        public User(string name, int age, string login, string email, string password, int id)
+        public User(string name, int age, string login, string email,string salt, string password, int id)
         {
             Name = name;
             Age = age;            
             Login = login;
             Email = email;
+            Salt = salt;
             Password = password;
             ID = id;
         }
         public void Print()
         {
-            Console.WriteLine($"Имя: {Name} Возраст: {Age}  Login {Login} Email {Email} Password {Password} ID {ID}");
+            Console.WriteLine($"Имя: {Name} Возраст: {Age}  Login {Login} Email {Email} hashString {Salt} Password {Password} ID {ID}");
         }
 
     }
