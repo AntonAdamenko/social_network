91d79730b186606548b3398a75d7d2e45244d5bb
Anton Adamenko <adamanton1@ya.ru>
2019-11-09 23:10:02 +0700

    hashClass работает при регистрации
    Сохранение пароля: генерируем длинную случайную соль, присоединяем соль к введенному паролю и вычисляем хэш-код с помощью хэш-функции sha256, сохраняем в БД соль и хэш-код.

---
+++ src/homeWork6/Input/hashClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using homeWork6.Input.CheckData;
+
+namespace homeWork6.Input
+{
+    class hashClass
+    {     
+                
+        public static string hashPasswordRegistration(string salt)
+        {
+            var stringSalt = salt;
+            var inputPass = inputPassword.inputPasswordUser();
+            var hashPass = EncryptPassword(inputPass, stringSalt);
+                       
+            return hashPass;
+        }
+
+       
+        public static string salt(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (length-- > 0)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                    res.Append(valid[(int)(num % (uint)valid.Length)]);
+                }
+            }
+
+            return res.ToString();
+        }
+
+        
+        public static string EncryptPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = string.Format("{0}{1}", salt, password);
+                byte[] saltedPasswordAsBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                return Convert.ToBase64String(sha256.ComputeHash(saltedPasswordAsBytes));
+            }
+        }
+    }
+}
